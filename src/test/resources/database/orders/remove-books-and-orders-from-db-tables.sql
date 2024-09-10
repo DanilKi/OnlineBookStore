0a440d4765e82bf37d5e323c17b67d6bd0575b26
Danylo Kishchenko <amfich@gmail.com>
2024-09-11 00:24:30 +0300

    - added test cases for ShoppingCart entity (repository, service, controller); - added test cases for Order, OrderItem entities (repository, service, controller).

---
+++ src/test/resources/database/orders/remove-books-and-orders-from-db-tables.sql
@@ -0,0 +1,3 @@
+DELETE FROM order_items WHERE id IN (1, 2, 3);
+DELETE FROM orders WHERE id IN (1, 2, 3);
+DELETE FROM books WHERE id IN (1, 2, 3);
---
+++ src/test/resources/database/orders/remove-saved-order-from-db-tables.sql
@@ -0,0 +1,2 @@
+DELETE FROM order_items WHERE id = 4;
+DELETE FROM orders WHERE id = 3;
